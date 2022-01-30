51ef90f9a68d89d6b3b92498785b04e3857391d0
zat <zat-dev@users.noreply.github.com>
2022-01-30 17:19:01 +0900

    change simulator message

--- simulator.py
+++ simulator.py
@@ -17,15 +17,18 @@ def simulate_reply(input_word, answer):
 def simulate(answer):
     ans_candidates = all_5letter_words
     input_word = FIRST_ANSWER
+    print(f"------- start simulation for {answer} --------")
     for turn in range(1, MAX_TURN+1):
-        print(f"answer candidates: {len(ans_candidates)}")
-        print(f"trun{turn} input: {input_word}")
+        print(f"  answer candidates: {len(ans_candidates)}")
+        print(f"  turn{turn} input: {input_word}")
         reply = simulate_reply(input_word, answer)
         if reply == [LetterReply.CORRECT]*5:
-            print("solved")
-            break
+            print(" solved")
+            return turn
         ans_candidates = filter_by_reply(ans_candidates, input_word, reply)
         input_word = pararell_search(ans_candidates)
+    print(" failed")
+    return 7
 
 
 if __name__ == "__main__":
